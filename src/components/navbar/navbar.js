ccc448b1087531db792cd66cc5115d38779633bc
samijandali <samijandali18@gmail.com>
2023-12-22 13:21:30 -0500

    Navbar CSS Update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,21 +2,21 @@ import './navbar.css';
 import { Button, Col, Container, Form, InputGroup, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
 import githubLogo from '../../assets/logos/github.svg'
 import linkedInLogo from '../../assets/logos/linkedin.svg'
-import React from 'react';
 
 function NavBar() {
   return (
-    <Navbar className="bg-body-tertiary justify-content-between">
-      <Container>
-        <Navbar.Brand href="#home">Gabriel Ragy</Navbar.Brand>
-        <Nav className="titles">
-          <Nav.Link href="#link">About</Nav.Link>
-          <Nav.Link href="#techstack">Tech Stack</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <Button className='imageButton' src={githubLogo} alt="GitHub"></Button>
-          <img src={linkedInLogo} alt="GitHub"></img>
-        </Nav>
+    <Navbar className="my-navbar justify-content-between">
+      <Container >
+      <Navbar.Brand class="my-name" href="#home">Gabriel Ragy</Navbar.Brand>
+      <Nav className="titles">
+            <Nav.Link class="my-link" href="#home">Home</Nav.Link>
+            <Nav.Link class="my-link" href="#link">About</Nav.Link>
+            <Nav.Link class="my-link" href="#home">Tech Stack</Nav.Link>
+            <Nav.Link class="my-link" href="#link">Projects</Nav.Link>
+            <Nav.Link style={{paddingRight: "40px"}} class="my-link" href="#home">Contact</Nav.Link>
+            <img src={githubLogo} alt="GitHub"></img>
+            <img src={linkedInLogo} alt="GitHub"></img>
+          </Nav>
       </Container>
     </Navbar>
   );
